3e347f514c3efaeba204d8f7074c5cea0e954aa0
LoisOmodibo <lois@prioclen.com>
2025-11-01 03:00:12 +0100

    Fix: Updated resume

--- src/Components/Home/CTA.jsx
+++ src/Components/Home/CTA.jsx
@@ -1,28 +1,17 @@
-import React from 'react';
-import Cv from '../../assets/Lois-cv-2025.pdf';
-import styles from './Home.module.css'
-
+import React from "react";
+import Cv from "../../assets/Loisss.pdf";
+import styles from "./Home.module.css";
 
 export const CTA = () => {
   return (
-    // <div className={home.buttonContainer}>
-    // <a href={Cv} download>Download CV</a>
-    // <a href='#contact'>Let's Talk</a>
-    // </div>
+    <div className={styles.buttonGroup}>
+      <a href={Cv} download>
+        <button className={styles.primaryButton}>Get Resume</button>
+      </a>
 
-      <div className={styles.buttonGroup}>
-       <a href={Cv} download>
-                  <button className={styles.primaryButton}>
-                    Get Resume
-                  </button>
-                  </a>
-
-                  <a href='#contactme'>
-                  <button className={styles.secondaryButton}>
-                    Let's Talk
-                  </button>
-                  </a>
-                </div>
-  )
-}
- 
+      <a href="#contactme">
+        <button className={styles.secondaryButton}>Let's Talk</button>
+      </a>
+    </div>
+  );
+};
